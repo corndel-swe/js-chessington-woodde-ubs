fd7ff43ecb5b8ab1796838973d6e90d101b7b0a5
Andy Huang <cerberus@ayxh.dev>
2024-05-13 12:09:12 +0000

    Add bishop and king specs

--- tests/models/pieces/bishop.spec.js
+++ tests/models/pieces/bishop.spec.js
@@ -1,3 +1,103 @@
 import Bishop from '../../../src/models/pieces/bishop.js'
+import Pawn from '../../../src/models/pieces/pawn.js'
+import King from '../../../src/models/pieces/king.js'
+import Player from '../../../src/models/player.js'
+import Square from '../../../src/models/square.js'
+import Board from '../../../src/models/board.js'
 
-describe('Bishop', () => {})
+describe('Bishop', () => {
+  let board
+  beforeEach(() => (board = new Board()))
+
+  it('can move diagonally', () => {
+    const bishop = new Bishop(Player.WHITE)
+    board.setPiece(Square.at(2, 3), bishop)
+
+    const moves = bishop.getAvailableMoves(board)
+
+    const expectedMoves = [
+      // Forwards diagonal
+      Square.at(0, 1),
+      Square.at(1, 2),
+      Square.at(3, 4),
+      Square.at(4, 5),
+      Square.at(5, 6),
+      Square.at(6, 7),
+      // Backwards diagonal
+      Square.at(0, 5),
+      Square.at(1, 4),
+      Square.at(3, 2),
+      Square.at(4, 1),
+      Square.at(5, 0),
+    ]
+
+    assert.deepEqual(new Set(moves), new Set(expectedMoves))
+  })
+
+  it('cannot make any other moves', () => {
+    const bishop = new Bishop(Player.WHITE)
+    board.setPiece(Square.at(2, 3), bishop)
+
+    const moves = bishop.getAvailableMoves(board)
+
+    assert.equal(moves.length, 11, '`moves` does not have length 11')
+  })
+
+  it('cannot move through friendly pieces', () => {
+    const bishop = new Bishop(Player.WHITE)
+    const friendlyPiece = new Pawn(Player.WHITE)
+    board.setPiece(Square.at(4, 4), bishop)
+    board.setPiece(Square.at(6, 6), friendlyPiece)
+
+    const moves = bishop.getAvailableMoves(board)
+
+    assert(!moves.some(Square.at(7, 7)), '`moves` contains the square (7, 7)')
+  })
+
+  it('cannot move through opposing pieces', () => {
+    const bishop = new Bishop(Player.WHITE)
+    const opposingPiece = new Pawn(Player.BLACK)
+    board.setPiece(Square.at(4, 4), bishop)
+    board.setPiece(Square.at(6, 6), opposingPiece)
+
+    const moves = bishop.getAvailableMoves(board)
+
+    assert(!moves.some(Square.at(7, 7)), '`moves` contains the square (7, 7)')
+  })
+
+  it('can take opposing pieces', () => {
+    const bishop = new Bishop(Player.WHITE)
+    const opposingPiece = new Pawn(Player.BLACK)
+    board.setPiece(Square.at(4, 4), bishop)
+    board.setPiece(Square.at(6, 6), opposingPiece)
+
+    const moves = bishop.getAvailableMoves(board)
+
+    assert(
+      moves.some(Square.at(6, 6)),
+      '`moves` does not contain the square (6, 6)'
+    )
+  })
+
+  it('cannot take the opposing king', () => {
+    const bishop = new Bishop(Player.WHITE)
+    const opposingKing = new King(Player.BLACK)
+    board.setPiece(Square.at(4, 4), bishop)
+    board.setPiece(Square.at(6, 6), opposingKing)
+
+    const moves = bishop.getAvailableMoves(board)
+
+    assert(!moves.some(Square.at(6, 6)), '`moves` contains the square (6, 6)')
+  })
+
+  it('cannot take friendly pieces', () => {
+    const bishop = new Bishop(Player.WHITE)
+    const friendlyPiece = new Pawn(Player.WHITE)
+    board.setPiece(Square.at(4, 4), bishop)
+    board.setPiece(Square.at(6, 6), friendlyPiece)
+
+    const moves = bishop.getAvailableMoves(board)
+
+    assert(!moves.some(Square.at(6, 6)), '`moves` contains the square (6, 6)')
+  })
+})
--- tests/models/pieces/king.spec.js
+++ tests/models/pieces/king.spec.js
@@ -1,3 +1,86 @@
 import King from '../../../src/models/pieces/king.js'
+import Pawn from '../../../src/models/pieces/pawn.js'
+import Board from '../../../src/models/board.js'
+import Player from '../../../src/models/player.js'
+import Square from '../../../src/models/square.js'
 
-describe('King', () => {})
+describe('King', () => {
+  let board
+  beforeEach(() => (board = new Board()))
+
+  it('can move to adjacent squares', () => {
+    const king = new King(Player.WHITE)
+    board.setPiece(Square.at(3, 4), king)
+
+    const moves = king.getAvailableMoves(board)
+
+    const expectedMoves = [
+      Square.at(2, 3),
+      Square.at(2, 4),
+      Square.at(2, 5),
+      Square.at(3, 5),
+      Square.at(4, 5),
+      Square.at(4, 4),
+      Square.at(4, 3),
+      Square.at(3, 3),
+    ]
+
+    assert.deepEqual(new Set(moves), new Set(expectedMoves))
+  })
+
+  it('cannot make any other moves', () => {
+    const king = new King(Player.WHITE)
+    board.setPiece(Square.at(3, 4), king)
+
+    const moves = king.getAvailableMoves(board)
+
+    assert.equal(moves.length, 8, '`moves` does not have length 8')
+  })
+
+  it('cannot leave the board', () => {
+    const king = new King(Player.WHITE)
+    board.setPiece(Square.at(0, 0), king)
+
+    const moves = king.getAvailableMoves(board)
+
+    const expectedMoves = [Square.at(0, 1), Square.at(1, 1), Square.at(1, 0)]
+
+    assert.deepEqual(new Set(moves), new Set(expectedMoves))
+  })
+
+  it('can take opposing pieces', () => {
+    const king = new King(Player.WHITE)
+    const opposingPiece = new Pawn(Player.BLACK)
+    board.setPiece(Square.at(4, 4), king)
+    board.setPiece(Square.at(5, 5), opposingPiece)
+
+    const moves = king.getAvailableMoves(board)
+
+    assert(
+      moves.some(Square.at(5, 5)),
+      '`moves` does not contain the square (5, 5)'
+    )
+  })
+
+  it('cannot take the opposing king', () => {
+    const king = new King(Player.WHITE)
+    const opposingKing = new King(Player.BLACK)
+    board.setPiece(Square.at(4, 4), king)
+    board.setPiece(Square.at(5, 5), opposingKing)
+
+    const moves = king.getAvailableMoves(board)
+
+    assert(!moves.some(Square.at(5, 5)), '`moves` contains the square (5, 5)')
+  })
+
+  it('cannot take friendly pieces', () => {
+    const king = new King(Player.WHITE)
+    const friendlyPiece = new Pawn(Player.WHITE)
+    board.setPiece(Square.at(4, 4), king)
+    board.setPiece(Square.at(5, 5), friendlyPiece)
+
+    const moves = king.getAvailableMoves(board)
+
+    assert(!moves.some(Square.at(5, 5)), '`moves` contains the square (5, 5)')
+  })
+})
